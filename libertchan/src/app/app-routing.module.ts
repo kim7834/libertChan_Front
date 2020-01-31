17725fa3de102e7d19828fe9ad6955dd1c0887d1
Boris BELLOC <bb.itwork@gmail.com>
2020-01-31 11:53:03 +0100

    generic routing for navbar ok + exemple of route methods

--- libertchan/src/app/app-routing.module.ts
+++ libertchan/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { HeaderComponent } from './navigation/header/header.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'home' , pathMatch: 'full'},
   // { path: 'home' , component: NavbarComponent},
+
+  { path: 'chan/:shortName' , component: HeaderComponent},
+  // { path: 'chan/ac' , component: HeaderComponent},
+  // { path: 'chan/po' , component: HeaderComponent},
 ];
 
 @NgModule({
--- libertchan/src/app/navigation/header/header.component.ts
+++ libertchan/src/app/navigation/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChannelService } from '../service/channel.service';
 import { Channel } from 'src/app/models/channel';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,14 +10,31 @@ import { Observable } from 'rxjs';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  constructor(private channelService: ChannelService) {}
+  constructor(
+    private channelService: ChannelService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   currentChannel: Channel;
-  currentChannelObs: Observable<Channel>;
+  private routeSub: Subscription;
 
   ngOnInit() {
-    this.channelService.getChannelByShortName('ac').subscribe(response => {
-      this.currentChannel = response as Channel;
+    console.log(this.router.url);
+    console.log(this.router.config);
+
+    console.log('-----------');
+
+    this.routeSub = this.route.params.subscribe(params => {
+      console.log(params); //log the entire params object
+      console.log(params.shortName); //log the value of id
+
+
+
+      this.channelService.getChannelByShortName(params.shortName).subscribe(response => {
+        this.currentChannel = response as Channel;
+      });
     });
+
   }
 }
