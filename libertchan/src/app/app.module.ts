2634408ade8871663b4ac53c82da69bbc5b0905b
Boris BELLOC <bb.itwork@gmail.com>
2020-01-31 15:40:50 +0100

    home route ok

--- libertchan/src/app/app.module.ts
+++ libertchan/src/app/app.module.ts
@@ -7,11 +7,18 @@ import { NavigationModule } from './navigation/navigation.module';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navigation/navbar/navbar.component';
 import { HeaderComponent } from './navigation/header/header.component';
+
+
+import { HomeModule } from './home/home.module';
 import { CatalogModule } from './catalog/catalog.module';
+
+import { HomeComponent } from './home/home/home.component';
 import { CatalogDetailComponent } from './catalog/catalog-detail/catalog-detail.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/chan/ac', pathMatch: 'full' },
+  // { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '' , component: HomeComponent},
+
   { path: 'chan/:shortName', component: CatalogDetailComponent }
 ];
 
@@ -21,6 +28,7 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     NavigationModule,
+    HomeModule,
     CatalogModule,
     RouterModule.forRoot(appRoutes)
   ],
