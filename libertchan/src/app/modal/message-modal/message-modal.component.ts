d9684ddad5a5d3cb08821e4aec77af019098bcb6
Nicolas <nicolas.gamel12450@gmail.com>
2020-02-13 15:04:33 +0100

    création du composant du modal pour les messages

---
+++ libertchan/src/app/modal/message-modal/message-modal.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-message-modal',
+  templateUrl: './message-modal.component.html',
+  styleUrls: ['./message-modal.component.scss']
+})
+export class MessageModalComponent implements OnInit {
+  myForm: FormGroup;
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {}
+
+  private createForm() {
+    this.myForm = this.formBuilder.group({
+      content: '',
+      imageLink: ''
+    });
+  }
+
+  private submitForm() {
+    this.activeModal.close(this.myForm.value);
+  }
+}
