f42e9d94239f8cefe6fef0e2c1e32ad3f5413c09
Nicolas <nicolas.gamel12450@gmail.com>
2020-02-17 14:25:09 +0100

    les messages peuvent être personnalisés

--- libertchan/src/app/topic/message/message.component.ts
+++ libertchan/src/app/topic/message/message.component.ts
@@ -12,4 +12,15 @@ export class MessageComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  getTextContent() {
+    this.textContent = this.textContent.replace('<lien>', '<a href="');
+    this.textContent = this.textContent.replace(
+      '</lien>',
+      '" target="_blank">lien</a>'
+    );
+
+    //console.log(this.textContent.split('\n'));
+    return this.textContent.split('\n');
+  }
 }
