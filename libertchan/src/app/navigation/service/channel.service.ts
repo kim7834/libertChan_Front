3e0b250292816ec29c6705bc21fa3f2376cbc5f1
Nicolas <nicolas.gamel12450@gmail.com>
2020-02-17 15:27:01 +0100

    commit before pulling

--- libertchan/src/app/navigation/service/channel.service.ts
+++ libertchan/src/app/navigation/service/channel.service.ts
@@ -7,12 +7,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 // import data from '../../../../urlconfig.json';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class ChannelService {
-  private baseUrl = 'http://localhost:8080/api/channels';
+  private baseUrl = 'http://192.168.1.89:8080/api/channels';
   // private baseUrl = 'http://192.168.1.22:8080/api/channels';
 
   constructor(private httpClient: HttpClient) {}
