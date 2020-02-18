03e7b93ae75744f9a51953074899155734dff716
Nicolas <nicolas.gamel12450@gmail.com>
2020-02-18 15:05:46 +0100

    fix

--- libertchan/src/app/services/channel.service.ts
+++ libertchan/src/app/services/channel.service.ts
@@ -11,8 +11,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ChannelService {
-  private baseUrl = 'http://localhost:8080/api/channels';
-  // private baseUrl = 'http://192.168.1.89:8080/api/channels';
+  // private baseUrl = 'http://localhost:8080/api/channels';
+  private baseUrl = 'http://192.168.1.89:8080/api/channels';
   // private baseUrl = 'http://192.168.1.22:8080/api/channels';
 
   constructor(private httpClient: HttpClient) {}
