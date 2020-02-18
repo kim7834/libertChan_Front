496514f3c51ee59beb5becb4e4f4d601fe5f358a
Boris BELLOC <bb.itwork@gmail.com>
2020-02-18 16:53:28 +0100

    minor updates

--- libertchan/src/app/topic/message/message.component.ts
+++ libertchan/src/app/topic/message/message.component.ts
@@ -18,19 +18,6 @@ export class MessageComponent implements OnInit {
 
   constructor() {}
 
-  // over($event) {
-  //   const parent = $event.fromElement;
-  //   let imgSrc = $event.srcElement.src;
-  //   const duplicatedImage = document.createElement('div');
-  //   duplicatedImage.className = 'duplicatedImgExpand';
-  //   duplicatedImage.innerHTML = '<img src=' + imgSrc + '>';
-
-  //   parent.appendChild(duplicatedImage);
-  // }
-
-  // removeIt(){
-  // }
-
 
   ngOnInit() {
   }
