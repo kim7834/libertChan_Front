02dfa87455e1da68f8157e1f6ecda70adaddc2ed
Boris BELLOC <bb.itwork@gmail.com>
2020-02-19 21:53:50 +0100

    LOAD PREFERENCE FROM DB ON PAGE LOADING OK

--- libertchan/src/app/customization/themes/themes.component.ts
+++ libertchan/src/app/customization/themes/themes.component.ts
@@ -3,6 +3,12 @@ import { StorageMap } from '@ngx-pwa/local-storage';
 
 import { ThemeService } from '../../services/theme.service';
 import { Subscription } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+interface Option {
+  name: string;
+  prop: string;
+}
 
 @Component({
   selector: 'app-themes',
@@ -10,49 +16,71 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./themes.component.scss']
 })
 export class ThemesComponent implements OnInit {
-  constructor(
-    private storage: StorageMap,
-  ) {}
-
-
+  constructor(private storage: StorageMap, private fb: FormBuilder) {}
 
   // themeSelected: string;
-  themeSelected: string;
+  themeSelected: any;
+  // options: FormGroup;
 
-  // TODO: dans un service
-  userPreferences = { themeSelected: this.themeSelected };
+  userPreferences = {
+    theme: this.themeSelected
+  };
 
   options = [
-    { name: 'Défault', value: 0 },
-    { name: 'HotPink', value: 1 }
+    { name: 'Défault' },
+    { name: 'HotPink' }
   ];
 
-
-
   changeTheme() {
-    this.userPreferences.themeSelected = this.themeSelected;
+    this.userPreferences.theme = this.themeSelected;
+    this.savePreference();
   }
 
   savePreference() {
-    this.changeTheme();
-    // console.log('theme ', this.themeSelected);
     this.storage
       .set('userPreferences', this.userPreferences)
       .subscribe(() => {});
-    // console.log('b', this.themesOption);
-    // this.storage.set('themesOption', this.themesOption).subscribe(() => {});
+    console.log('saved ', this.userPreferences.theme);
   }
+
+  // getIt() {
+  //   this.storage.get('userPreferences').map(
+  //     (response) => { return response.json()}
+  //   );
+  // }
+
   getPreference() {
     this.storage.get('userPreferences').subscribe(preference => {
       // Save default preference on first visit
+      console.log('preference AVANT', preference);
       if (preference === undefined) {
         this.themeSelected = 'Défault';
-        this.savePreference();
+        this.changeTheme();
       }
+      this.themeSelected = preference.theme;
+      console.log('preference APRES ', preference.theme);
+      // for (let entry of preference) {
+      //   console.log('entry ', entry);
+      // }
+      // this.themeSelected = preference as string;
     });
   }
 
   ngOnInit() {
+    // this.themeSelected = 'Défault';
+
+    // this.themeSelected = this.getPreference();
     this.getPreference();
+    // this.themeSelected = this.getPreference();
+    // console.log('init ' , this.themeSelected );
+
+    // this.options = this.fb.group({
+    //   options: [null, Validators.required]
+    // });
+
+    // const toSelect = this.options.find(c => c.name === 'HotPink');
+    // toSelect.get('themeSelected').setValue(toSelect);
+    // this.themeSelected.setValue('HotPink');
+
   }
 }
