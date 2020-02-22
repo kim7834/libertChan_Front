17047747dbdcaba4e9bf9765e22e5e59c680111f
Boris BELLOC <bb.itwork@gmail.com>
2020-02-22 12:33:18 +0100

    themes edited

--- libertchan/src/app/services/message.service.ts
+++ libertchan/src/app/services/message.service.ts
@@ -9,8 +9,8 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class MessageService {
-  // private baseUrl = 'http://localhost:8080/api/messages';
-  private baseUrl = 'http://192.168.1.89:8080/api/messages';
+  private baseUrl = 'http://localhost:8080/api/messages';
+  // private baseUrl = 'http://192.168.1.89:8080/api/messages';
   // private baseUrl = 'http://192.168.1.22:8080/api/messages';
 
   constructor(private httpClient: HttpClient) {}
